2266edb56103b676f560de0c81a0a04755980a81
bertrandds-cmd <bertrand.ds@gmail.com>
2026-08-16 22:33:26 +0200

    Revert "chore: remove AdSense from TirageSimple"
    This reverts commit 8fa834f1c214604211f4c990db756c672d6c138d.

--- src/scripts/common.ts
+++ src/scripts/common.ts
@@ -1,6 +1,7 @@
 import { formatCount, parseList } from '../lib/lists';
 import { trackEvent } from '../lib/analytics';
 import { readConsent } from '../lib/consent';
+import { ADSENSE_CLIENT } from '../config/monetization';
 
 function loadAdsIfConsented(): void {
   if (readConsent()?.advertising !== true || !ADSENSE_CLIENT) return;
